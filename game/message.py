14257b9bd7793c731d1d78acd69667c927de8251
MarioMatheus <mariomatheu@gmail.com>
2020-02-02 21:00:46 -0300

    removed pycache, correcting spaces, add broadcast message handle in room

--- game/message.py
+++ game/message.py
@@ -1,16 +1,15 @@
 class GameMessage:
+    def encode(self, obj, module):
+        data = module + '|' + str(obj)
+        return data.encode()
 
-  def encode(self, obj, module):
-    data = module + '|' + str(obj)
-    return data.encode()
-
-  def decode(self, data):
-    module, payload = data.decode('utf-8').split('|')
-    str_data = str(
-      payload
-    ).replace(' ', '').replace('{', '').replace('}', '').split(',')
-    payload = {}
-    for attr in str_data:
-      key, value = attr.split(':')
-      payload[key.replace("'", '')] = value.replace("'", '')
-    return module, payload
+    def decode(self, data):
+        module, payload = data.decode('utf-8').split('|')
+        str_data = str(
+            payload
+        ).replace(' ', '').replace('{', '').replace('}', '').split(',')
+        payload = {}
+        for attr in str_data:
+            key, value = attr.split(':')
+            payload[key.replace("'", '')] = value.replace("'", '')
+        return module, payload
---
+++ game/room.py
@@ -0,0 +1,38 @@
+import logging
+from . import message
+
+class Chat:
+    def __init__(self, users):
+        self.users = users
+        self.messages = []
+
+    def broadcast_message(self, message_data, client):
+        for user in self.users:
+            if user != client:
+                user.send(message)
+
+class Room:
+    def __init__(self, players):
+        self.players = players
+        self.chat = Chat(players)
+        self.message = message.GameMessage()
+    
+    def receive_data(self, data, from_client):
+        module, payload = self.message.decode(data)
+        if module == 'CHAT':
+            self.receive_chat_msg(payload, from_client)
+        elif module == 'MATCH':
+            pass
+
+    def receive_chat_msg(self, payload, client):
+        try:
+            message_data = self.message.encode(payload, 'CHAT')
+            self.chat.broadcast_message(message_data, client)
+        except:
+            client.close()
+            self.remove_connection(client)
+
+    def remove_connection(self, connection):
+        if connection in self.players:
+            self.players.remove(connection)
+            logging.info('Removed player!')
